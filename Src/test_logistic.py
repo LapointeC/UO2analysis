3f08493212142b575d7c81909d48693e6d0f2a1a
Clovis Lapointe <lapointe@2a02-8429-aea6-4f01-2412-5141-02c7-b220.rev.sfr.net>
2024-05-14 10:01:41 +0200

    visualisation is working

--- Src/test_logistic.py
+++ Src/test_logistic.py
@@ -4,6 +4,7 @@ import pickle
 import shutil
 
 sys.path.append(os.getcwd())
+from matplotlib import colors
 
 from ase import Atoms
 from milady import * 
@@ -14,9 +15,11 @@ from my_cfg_reader import my_cfg_reader
 from library_mcd import Dfct_analysis_object, custom_writer
 
 from ovito.io.ase import ase_to_ovito
-from ovito.pipeline import StaticSource, Pipeline, PythonSource, PipelineSourceInterface
+from ovito.pipeline import Pipeline, PythonSource, PipelineSourceInterface
 from ovito.io import *
 from ovito.data import DataCollection
+
+from ovito.modifiers import ExpressionSelectionModifier
 
 from typing import List, Dict
 from PySide6.QtCore import QEventLoop
@@ -55,17 +58,54 @@ else :
 ## FRAME INTERFACE FOR OVITO
 ########################################################
 class FrameOvito(PipelineSourceInterface) : 
+    """Define the PipelineSourceInterface given to ovito pipeline to 
+    plot frame by frame the confiugrations"""
     def __init__(self, list_atoms : List[Atoms]) : 
         self.list_atoms = list_atoms
         self.lenght = len(list_atoms)
 
     def compute_trajectory_length(self, **kwargs):
+        """Needed method for pipeline"""
         return self.lenght
     
     def create(self, data : DataCollection, *, frame : int, **kwargs) : 
+        """Creator for DataCollection object inside the ovito pipeline"""
         data_atoms : DataCollection = ase_to_ovito(self.list_atoms[frame])
         data.particles = data_atoms.particles
         data.cell = data_atoms.cell
+
+class LogisticModifier :
+    """Ovito modifier function for visual logistic selection"""
+    def __init__(self, init_transparency : float = 0.9, threshold : float = 0.5, dict_color : Dict[int,str] = None) :
+        self.init_transparency = init_transparency
+        self.threshold = threshold
+        self.dict_color = dict_color
+        
+        if self.dict_color is not None : 
+            self._build_rgb_dict()
+
+    def _build_rgb_dict(self) -> None : 
+        self.dic_rgb = {}
+        for key in self.dict_color.keys() : 
+            self.dic_rgb[key] = colors.to_rgb(self.dict_color[key])
+
+    def AssignColors(self, frame : int, data : DataCollection) : 
+        particules_type = data.particles['Particle Type'][:]
+        color_array = np.empty((len(particules_type),3))
+        for id ,type in enumerate(particules_type) : 
+            color_array[id, :] = self.dic_rgb[type] 
+        data.particles_.create_property('Color',data=color_array)
+
+    def PerformLogisticSelection(self, frame : int, data : DataCollection) : 
+        logistic_score = data.particles['logistic-score'][:]
+        array_transparency = np.empty((logistic_score.shape[0],))
+        for id ,logi_score in enumerate(logistic_score) : 
+            if logi_score[1] > self.threshold : 
+                array_transparency[id] = (1.0 - logi_score[1])
+            else : 
+                array_transparency[id] = self.init_transparency 
+        data.particles_.create_property('Transparency',data=array_transparency)
+
 
 for id,key in enumerate(previous_dbmodel.model_init_dic.keys()) :
     print('... Analysis for : {:s} ...'.format(key))
@@ -81,10 +121,15 @@ for id,key in enumerate(previous_dbmodel.model_init_dic.keys()) :
         atoms_config.append(atom_config)
 
 frame_object = FrameOvito(atoms_config)
+logistic_modifier = LogisticModifier(threshold=0.7,
+                                     init_transparency=0.9,
+                                     dict_color={1:'lightcoral',2:'chartreuse'})
+
 pipeline_config = Pipeline(source = PythonSource(delegate=frame_object))
+pipeline_config.modifiers.append(logistic_modifier.PerformLogisticSelection)
+pipeline_config.modifiers.append(logistic_modifier.AssignColors)
 for frame in range(pipeline_config.source.num_frames) : 
     data = pipeline_config.compute(frame)
-    print(data.particles)
 
 pipeline_config.add_to_scene()
 
